7ecf80264784b87ee3798b0ee4ccaab4a9d536b2
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2022-06-25 18:27:12 +0530

    tut 21 done

--- pages/blog.js
+++ pages/blog.js
@@ -2,15 +2,12 @@ import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
 
-const Blog = () => {
-  const [blog, setBlog] = useState([])
-  useEffect(() => {
-    fetch('http://localhost:3000/api/blogs').then(a => {
-      return a.json()
-    }).then(data => {
-      setBlog(data);
-    })
-  }, [])
+const Blog = (props) => {
+  const [blog, setBlog] = useState(props.allblogs)
+  console.log(props)
+  // useEffect(() => {
+
+  // }, [])
 
   return (
     <div className={styles.container}>
@@ -29,6 +26,15 @@ const Blog = () => {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  let data = await fetch('http://localhost:3000/api/blogs')
+  let allblogs = await data.json()
+
+  return {
+    props: { allblogs }, // will be passed to the page component as props
+  }
 }
 
 export default Blog
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -2,19 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/Blogpost.module.css'
 
-const Slug = () => {
-    const [blog, setBlog] = useState();
-    const router = useRouter();
-    useEffect(() => {
-        if (!router.isReady) return;
-        const { slug } = router.query;
-        fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then(a => {
-            return a.json()
-        }).then(data => {
-            setBlog(data);
-        })
-    }, [router.isReady])
-
+const Slug = (props) => {
+    const [blog, setBlog] = useState(props.myblog);
 
     return (
         <div className={styles.container}>
@@ -28,6 +17,16 @@ const Slug = () => {
             </main>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const { slug } = context.query;
+    let data = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`)
+    let myblog = await data.json()
+
+    return {
+        props: { myblog }, // will be passed to the page component as props
+    }
 }
 
 export default Slug
