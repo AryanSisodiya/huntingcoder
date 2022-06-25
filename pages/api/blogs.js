7bf814fd978b3ee2287f733811f0ba3f2cf1a12f
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2022-06-25 17:23:16 +0530

    tut 19 done

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,9 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir(`blogdata`, (err, data) => {
-    console.log(req.query.slug);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+
+  let data = await fs.promises.readdir("blogdata");
+  // res.status(200).json(data);
+  let myfile;
+
+  let allBlogs = [];
+  // data.forEach(async item => {
+  //   console.log(item)
+  //   myfile = await fs.promises.readFile('blogdata/' + item, 'utf-8');
+  //   console.log(myfile)
+
+  //   allBlogs.push(JSON.parse(myfile))
+  // })
+
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item);
+    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json(allBlogs);
+
+  // fs.readdir(`blogdata`, (err, data) => {
+  //   console.log(req.query.slug);
+  //   })
+
+  // });
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
 
 const Blog = () => {
+  const [blog, setBlog] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/api/blogs').then(a => {
+      return a.json()
+    }).then(data => {
+      setBlog(data);
+    })
+  }, [])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         {/* <h1>Latest Blogs</h1> */}
-        <div>
-          <Link href={'/blogpost/learn-javascript'}>
-            <h3 className={styles.blogItemsh3}>How to learn Js in 2022</h3>
-          </Link>
-          <p>js is the language desinged for the frontend logic in the beginning and currently also used in the backend</p>
-        </div>
-        <div>
-          <Link href={'/blogpost/learn-javascript'}>
-            <h3 className={styles.blogItemsh3}>How to learn Js in 2022</h3>
-          </Link>
-          <p>js is the language desinged for the frontend logic in the beginning and currently also used in the backend</p>
-        </div>
-        <div>
-          <Link href={'/blogpost/learn-javascript'}>
-            <h3 className={styles.blogItemsh3}>How to learn Js in 2022</h3>
-          </Link>
-          <p>js is the language desinged for the frontend logic in the beginning and currently also used in the backend</p>
-        </div>
+        {blog.map(blogitem => {
+          return <div key={blogitem.slug}>
+            <Link href={`/blogpost/${blogitem.slug}`}>
+              <h3 className={styles.blogItemsh3}>{blogitem.title}</h3>
+            </Link>
+            <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+            <Link href={`/blogpost/${blogitem.slug}`}>
+              <span className={styles.btn}>Read More</span></Link>
+          </div>
+        })}
       </main>
     </div>
   )
