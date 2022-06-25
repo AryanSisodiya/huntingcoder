a8c7ff0cbb2abd7b9d4cd238665767f1fd123150
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2022-06-25 19:32:55 +0530

    tut 24 done

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
-import * as fs from 'fs'
 
 const Blog = (props) => {
   const [blog, setBlog] = useState(props.allblogs)
@@ -19,7 +18,7 @@ const Blog = (props) => {
             <Link href={`/blogpost/${blogitem.slug}`}>
               <h3 className={styles.blogItemsh3}>{blogitem.title}</h3>
             </Link>
-            <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+            <p className={styles.blogItemp}>{blogitem.metadesc.substr(0,140)}...</p>
             <Link href={`/blogpost/${blogitem.slug}`}>
               <span className={styles.btn}>Read More</span></Link>
           </div>
@@ -29,29 +28,29 @@ const Blog = (props) => {
   )
 }
 
-export async function getStaticProps(context) {
-  let data = await fs.promises.readdir("blogdata");
-  let myfile;
-  let allblogs = [];
-  for (let index = 0; index < data.length; index++) {
-    const item = data[index];
-    console.log(item)
-    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
-    allblogs.push(JSON.parse(myfile))
-  }
-
-  return {
-    props: { allblogs }, // will be passed to the page component as props
-  }
-}
-
-// export async function getServerSideProps(context) {
-//   let data = await fetch('http://localhost:3000/api/blogs')
-//   let allblogs = await data.json()
+// export async function getStaticProps(context) {
+//   let data = await fs.promises.readdir("blogdata");
+//   let myfile;
+//   let allblogs = [];
+//   for (let index = 0; index < data.length; index++) {
+//     const item = data[index];
+//     console.log(item)
+//     myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+//     allblogs.push(JSON.parse(myfile))
+//   }
 
 //   return {
 //     props: { allblogs }, // will be passed to the page component as props
 //   }
 // }
+
+export async function getServerSideProps(context) {
+  let data = await fetch('http://localhost:3000/api/blogs')
+  let allblogs = await data.json()
+
+  return {
+    props: { allblogs }, // will be passed to the page component as props
+  }
+}
 
 export default Blog
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/Blogpost.module.css'
-import * as fs from 'fs';
 
 const Slug = (props) => {
+    function createMarkup(c) {
+        return { __html: c }
+    }
+
     const [blog, setBlog] = useState(props.myblog);
 
     return (
@@ -12,44 +15,42 @@ const Slug = (props) => {
                 <h1>Title of the page: {blog && blog.title}</h1>
                 <h2 className={styles.author}>(Written by: {blog && blog.author})</h2>
                 <hr />
-                <div>
-                    {blog && blog.content}
-                </div>
+                {blog && <div dangerouslySetInnerHTML={createMarkup(blog.content)}></div>}
             </main>
         </div>
     )
 }
 
-// export async function getServerSideProps(context) {
-//     const { slug } = context.query;
-//     let data = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`)
-//     let myblog = await data.json()
-
-//     return {
-//         props: { myblog }, // will be passed to the page component as props
-//     }
-// }
-
-export async function getStaticProps(context) {
-    const {slug} = context.params;
-
-    let myblog = await fs.promises.readFile(`blogdata/${slug}.json`, 'utf-8')
-
+export async function getServerSideProps(context) {
+    const { slug } = context.query;
+    let data = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`)
+    let myblog = await data.json()
 
     return {
-        props: { myblog: JSON.parse(myblog) }, // will be passed to the page component as props
+        props: { myblog }, // will be passed to the page component as props
     }
 }
 
-export async function getStaticPaths() {
-    return {
-      paths: [
-        { params: { slug: "how-to-learn-flask" } },
-        { params: { slug: "how-to-learn-javascript" } },
-        { params: { slug: "how-to-learn-nextjs" } }
-      ],
-      fallback: true // false or 'blocking'
-    };
-  }
+// export async function getStaticProps(context) {
+//     const {slug} = context.params;
+
+//     let myblog = await fs.promises.readFile(`blogdata/${slug}.json`, 'utf-8')
+
+
+//     return {
+//         props: { myblog: JSON.parse(myblog) }, // will be passed to the page component as props
+//     }
+// }
+
+// export async function getStaticPaths() {
+//     return {
+//       paths: [
+//         { params: { slug: "how-to-learn-flask" } },
+//         { params: { slug: "how-to-learn-javascript" } },
+//         { params: { slug: "how-to-learn-nextjs" } }
+//       ],
+//       fallback: true // false or 'blocking'
+//     };
+//   }
 
 export default Slug
